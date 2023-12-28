25c40fc191407e8833c312eda3c9c8f1c4dffd8f
TonNQ <nguyenquoctoan12122003@gmail.com>
2023-12-28 09:36:47 +0700

    remove yarn.lock

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const peer_1 = require("peer");
-(0, peer_1.PeerServer)({ port: 9001, path: "/" });
+(0, peer_1.PeerServer)({ port: (process.env.PORT || 1337), path: "/" });
